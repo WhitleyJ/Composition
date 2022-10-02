e5a9b2d735e20781101c923b8522d93e8249dbf0
WhitliyJ <shepelev.yaros1@gmail.com>
2022-10-02 20:03:46 +0300

    data binding in GameFragment

--- app/src/main/java/com/composit/composition/presentation/BindingAdapter.kt
+++ app/src/main/java/com/composit/composition/presentation/BindingAdapter.kt
@@ -1,20 +1,25 @@
 package com.composit.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.composit.composition.R
 import com.composit.composition.domain.entities.GameResult
 
 @BindingAdapter("requiredAnswer")
-fun bindRequiredAnswer(textView: TextView,count: Int){
+fun bindRequiredAnswer(textView: TextView, count: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.required_answer),
         count
     )
 }// все правильно
+
 @BindingAdapter("score")
-fun bindRequiredScore(textView: TextView,count: Int){
+fun bindRequiredScore(textView: TextView, count: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.score),
         count
@@ -22,21 +27,22 @@ fun bindRequiredScore(textView: TextView,count: Int){
 } // все правильно
 
 @BindingAdapter("percent_of_Right")
-fun bindPercentReal(textView: TextView,percent: Int){
+fun bindPercentReal(textView: TextView, percent: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.percent),
         percent
     )
 }
+
 @BindingAdapter("percent")
-fun bindPercentScore(textView: TextView,gameResult: GameResult){
+fun bindPercentScore(textView: TextView, gameResult: GameResult) {
     textView.text = String.format(
         textView.context.getString(R.string.real_percent),
         getPercentOfRightAnswer(gameResult)
     )
 }
 
-private fun getPercentOfRightAnswer(gameResult:GameResult) = with(gameResult) {
+private fun getPercentOfRightAnswer(gameResult: GameResult) = with(gameResult) {
     if (countOfQuestions == 0) {
         0
     } else {
@@ -45,10 +51,51 @@ private fun getPercentOfRightAnswer(gameResult:GameResult) = with(gameResult) {
 }
 
 @BindingAdapter("emoji")
-fun bindEmoji(imageView: ImageView,count: Boolean){
-    when(count){
+fun bindEmoji(imageView: ImageView, count: Boolean) {
+    when (count) {
         true -> imageView.setImageResource(R.drawable.img3)
         false -> imageView.setImageResource(R.drawable.img4)
     }
 
 }
+
+@BindingAdapter("enoughCount")
+fun bindColor(textView: TextView, enough: Boolean) {
+    textView.setTextColor(parsColor(textView.context, enough))
+
+}
+
+@BindingAdapter("enoughPercent")
+fun bindColorProgress(progressBar: ProgressBar, enough: Boolean) {
+    val color = parsColor(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+
+}
+
+private fun parsColor(context: Context, goodState: Boolean): Int {
+    val color = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    val textColor = ContextCompat.getColor(context, color)
+    return textColor
+}
+
+@BindingAdapter("numberToText")
+fun numberToText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionItemClick")
+fun bindOnClickOption(textView: TextView,clickListener: OnOptionItemClick){
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+
+}
+
+interface OnOptionItemClick{
+    fun onOptionClick(option: Int)
+}
+
